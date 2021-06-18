fa054b9bc2645946b9117664953577cf51232f1a
Sowmyam22 <msowmyadeveloper@gmail.com>
2021-06-18 18:40:57 +0530

    restricting the route access using the middleware

--- routes/admin.js
+++ routes/admin.js
@@ -10,26 +10,27 @@ const {
     postEditProduct,
     postDeleteProduct
 } = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
 
 const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product', getAddProduct);
+router.get('/add-product', isAuth, getAddProduct);
 
 // /admin/products => GET
-router.get('/products', getProducts);
+router.get('/products', isAuth, getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', postAddProduct);
+router.post('/add-product', isAuth, postAddProduct);
 
 // /admin/edit-product => GET
-router.get('/edit-product/:productId', getEditProduct);
+router.get('/edit-product/:productId', isAuth, getEditProduct);
 
 // /admin/edit-product => POST
-router.post('/edit-product', postEditProduct);
+router.post('/edit-product', isAuth, postEditProduct);
 
 // /admin/delete-product => POST
-router.post('/delete-product', postDeleteProduct);
+router.post('/delete-product', isAuth, postDeleteProduct);
 
 exports.routes = router;
 exports.products = products;
